8549275cfdcfb7295150747636172bdaeba0c02d
immonel2 <leo.immonen@aalto.fi>
2021-02-16 20:03:02 +0200

    Removed eslint-header plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,10 @@
 module.exports = {
   extends: ['react-app'],
-  plugins: ['header', 'simple-import-sort'],
+  plugins: ['simple-import-sort'],
   rules: {
     // use sorting of simple-import-sort and disable others
     'sort-imports': 'off',
     'import/order': 'off',
     'simple-import-sort/sort': 'error',
-    // header
-    'header/header': [
-      2,
-      'block',
-      [
-        '*',
-        ' * ---------------------------------------------------------------------',
-        ' * Copyright (c) 2020 EclipseSource Munich',
-        ' * Licensed under MIT',
-        ' * https://github.com/eclipsesource/jsonforms-editor/blob/master/LICENSE',
-        ' * ---------------------------------------------------------------------',
-        ' ',
-      ],
-    ],
   },
 };
